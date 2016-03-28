36bb4c25557ed49dcc30bd3457e3e9cdffa78eae
George Schizas <gschizas@gmail.com>
2016-03-28 21:31:53 +0300

    Output to file

--- shell.py
+++ shell.py
@@ -45,30 +45,34 @@ english_layouts = [kbd for kbd in kbd_layouts if 'English' in kbd['name']]
 
 # with keyboard_dump.load_keyboard(kbd_layout['id']) as kbd:
 #with keyboard_dump.load_keyboard('04090409') as kbd:
-for kbd_id in ('04090409', '04080408', '04070407', '08090409'):
-    with keyboard_dump.load_keyboard(kbd_id) as kbd:
-        print('=' * 20, keyboard_dump.layout_name(kbd_id), '=' * 20)
-        for shift_state in (['normal'], ['shift']):  #, ['alt_gr']):
-            # shift_state = ['shift']  # ['control', 'alt_gr', 'shift']
-            print('--' * 10, '+'.join(shift_state), '--' * 10)
-            print('Row 1:', SEP.join(keyboard_dump.key_values(
-                kbd,
-                VK_OEM_3, VK_1, VK_2, VK_3, VK_4, VK_5, VK_6, VK_7, VK_8, VK_9, VK_0,
-                VK_OEM_MINUS, VK_OEM_PLUS,
-                shift_state=shift_state)))
-            print('Row 2:   ', SEP.join(keyboard_dump.key_values(
-                kbd,
-                VK_Q, VK_W, VK_E, VK_R, VK_T, VK_Y, VK_U, VK_I, VK_O, VK_P,
-                VK_OEM_4, VK_OEM_6, VK_OEM_5,
-                shift_state=shift_state)))
-            print('Row 3:    ', SEP.join(keyboard_dump.key_values(
-                kbd,
-                VK_A, VK_S, VK_D, VK_F, VK_G, VK_H, VK_J, VK_K, VK_L,
-                VK_OEM_1, VK_OEM_7, VK_OEM_8,
-                shift_state=shift_state)))
-            print('Row 4:     ', SEP.join(keyboard_dump.key_values(
-                kbd,
-                VK_Z, VK_X, VK_C, VK_V, VK_B, VK_N, VK_M,
-                VK_OEM_COMMA, VK_OEM_PERIOD, VK_OEM_2,
-                shift_state=shift_state)))
-            print()
+# for kbd_id in ('04090409', '04080408', '04070407', '08090409', '00020409'):
+
+with open('output.txt', 'w', encoding='utf_8_sig') as output_file:
+    for kbd_id in ('04090409', '00020409'):
+    # for kbd_id in ('00020409', ):    
+        with keyboard_dump.load_keyboard(kbd_id) as kbd:
+            print('=' * 20, keyboard_dump.layout_name(kbd_id), '=' * 20, file=output_file)
+            for shift_state in (['normal'], ['shift'], ['alt_gr'], ['shift', 'alt_gr']):
+                # shift_state = ['shift']  # ['control', 'alt_gr', 'shift']
+                print('--' * 10, '+'.join(shift_state), '--' * 10, file=output_file)
+                print('Row 1:', SEP.join(keyboard_dump.key_values(
+                    kbd,
+                    VK_OEM_3, VK_1, VK_2, VK_3, VK_4, VK_5, VK_6, VK_7, VK_8, VK_9, VK_0,
+                    VK_OEM_MINUS, VK_OEM_PLUS,
+                    shift_state=shift_state)), file=output_file)
+                print('Row 2:   ', SEP.join(keyboard_dump.key_values(
+                    kbd,
+                    VK_Q, VK_W, VK_E, VK_R, VK_T, VK_Y, VK_U, VK_I, VK_O, VK_P,
+                    VK_OEM_4, VK_OEM_6, VK_OEM_5,
+                    shift_state=shift_state)), file=output_file)
+                print('Row 3:    ', SEP.join(keyboard_dump.key_values(
+                    kbd,
+                    VK_A, VK_S, VK_D, VK_F, VK_G, VK_H, VK_J, VK_K, VK_L,
+                    VK_OEM_1, VK_OEM_7, VK_OEM_8,
+                    shift_state=shift_state)), file=output_file)
+                print('Row 4:     ', SEP.join(keyboard_dump.key_values(
+                    kbd,
+                    VK_Z, VK_X, VK_C, VK_V, VK_B, VK_N, VK_M,
+                    VK_OEM_COMMA, VK_OEM_PERIOD, VK_OEM_2,
+                    shift_state=shift_state)), file=output_file)
+                print('', file=output_file)
